3cb7a9a39d2c2fa7d687a8d69ef22f7ae2f70b11
DaanJG <daangilissen@gmail.com>
2018-09-05 14:10:28 +0200

    added get_one_pizza method

--- pizza_api.py
+++ pizza_api.py
@@ -13,6 +13,10 @@ pizzaDB = [
 def get_pizza():
     return jsonify({"pizzaDB": pizzaDB})
 
+@app.route("/<string:name>", methods=['GET'])
+def get_one_pizza(name):
+    resultPizza = [pizza for pizza in pizzaDB if pizza['name'] == name]
+    return jsonify({"pizzaDB": resultPizza})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
