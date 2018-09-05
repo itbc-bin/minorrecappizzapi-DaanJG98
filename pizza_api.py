259796b588757e7b67af09a5a25b48176a0cc504
DaanJG <daangilissen@gmail.com>
2018-09-05 15:33:12 +0200

    update pizza api

--- pizza_api.py
+++ pizza_api.py
@@ -3,9 +3,9 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 pizzaDB = [
-            {"name": "tonno"},
-            {"name": "salami"},
-            {"name": "hawaii"}
+            {"name": "tonno", "ingredienten":["tomaat", "kaas", "tonijn"]},
+            {"name": "salami", "ingredienten":["tomaat", "kaas", "salami"]},
+            {"name": "hawaii", "ingredienten":["tomaat", "kaas", "ham", "ananas"]}
             ]
 
 
@@ -17,6 +17,39 @@ def get_pizza():
 def get_one_pizza(name):
     resultPizza = [pizza for pizza in pizzaDB if pizza['name'] == name]
     return jsonify({"pizzaDB": resultPizza})
+
+@app.route("/<string:name>/ingredienten", methods=['GET'])
+def get_ingredienten(name):
+    resultPizza = [pizza['ingredienten'] for pizza in pizzaDB if pizza['name'] == name]
+    return jsonify({"pizzaDB": resultPizza})
+
+@app.route("/", methods=['POST'])
+def addOnePizza():
+    # pizza = {
+    #     'name': request.json['name'],
+    #     'ingredienten': request.json['ingredienten']
+    # }
+    pizzaDB.append(request.json)
+    return jsonify({'pizzaDB': pizzaDB})
+
+# add one or more ingredients to a single pizza
+@app.route("/add_ingredient/<string:name>", methods=['POST'])
+def add_ingredients(name):
+    ingredients = request.args.get('ingredients').split(',')
+    for pizza in pizzaDB:
+        if pizza['name'] == name:
+            for ingredient in ingredients:
+                pizza['ingredienten'].append(ingredient)
+    return jsonify({'pizzaDB': pizzaDB})
+
+# add a price to a pizza
+@app.route("/add_price/<string:name>", methods=['POST'])
+def add_price(name):
+    price = request.args.get('price')
+    for pizza in pizzaDB:
+        if pizza['name'] == name:
+            pizza.update({'price': price})
+    return jsonify({'pizzaDB': pizzaDB})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
